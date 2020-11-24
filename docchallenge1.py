784c1c5f410e0d372207b6b751580aefc493a125
5kyw41k3r <67189930+5kyw41k3r@users.noreply.github.com>
2020-11-24 09:11:49 +0530

    Update docchallenge1.py

--- docchallenge1.py
+++ docchallenge1.py
@@ -1,11 +1,18 @@
-x = input("Enter something: ")
-# Ask for user input
-try:
+import sys
+
+def main():
+  x = input("Enter something: ")
+  if x == "exit":
+    sys.exit()
+  try:
     if type(eval(x)) == float: # If statement for float identification
         print(x, " is floating point number") # Print data type
     elif type(eval(x)) == int: # elif statement for integer identification
         print(x, " is interger number") # Print data type    
     elif type(eval(x)) == bool: # elif statement for bool identification
         print(x, " is a boolean") # Print data type  
-except:
+  except:
     print("That is a string") # Print data type
+
+while main:
+  main()
